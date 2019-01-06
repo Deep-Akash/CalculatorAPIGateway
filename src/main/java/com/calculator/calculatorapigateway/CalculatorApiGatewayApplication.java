af85f5657bbe6322fcf368de61cdf5b28dc8a4db
Akashdeep <>
2019-01-06 21:16:54 +0530

    zuuling

--- src/main/java/com/calculator/calculatorapigateway/CalculatorApiGatewayApplication.java
+++ src/main/java/com/calculator/calculatorapigateway/CalculatorApiGatewayApplication.java
@@ -1,14 +1,38 @@
 package com.calculator.calculatorapigateway;
 
+import com.calculator.calculatorapigateway.filters.ErrorFilter;
+import com.calculator.calculatorapigateway.filters.PostFilter;
+import com.calculator.calculatorapigateway.filters.PreFilter;
+import com.calculator.calculatorapigateway.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class CalculatorApiGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
 }
 
---
+++ src/main/java/com/calculator/calculatorapigateway/filters/PreFilter.java
@@ -0,0 +1,33 @@
+package com.calculator.calculatorapigateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class PreFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        System.out.println("Inside PRE filter Run");
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        System.out.println("Request Method : " + request.getMethod() + " Request URL : " + request.getRequestURL().toString());
+        return null;    }
+}
