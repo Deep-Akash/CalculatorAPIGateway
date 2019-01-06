5a92f29b1c0c676f1abc3f9ad761e80de962d176
Akashdeep <>
2019-01-06 13:20:44 +0530

    firstever

---
+++ src/main/java/com/calculator/calculatorapigateway/CalculatorApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.calculator.calculatorapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApiGatewayApplication.class, args);
+    }
+
+}
+
